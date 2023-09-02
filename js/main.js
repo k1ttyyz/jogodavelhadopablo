8dee645e55a4446a0729c1c368ca0e7bbebf537d
pabloleonardoo <luckyxd136@gmail.com>
2023-09-02 20:10:17 -0300

    ATUALIZANDO JS
    criei uma função para mostrar quem ganhou , nela possui um array para mapear cada elemento da index  e mostrar segundo as posiçoes qual jogador ganhou ou se empatou

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ let posicoes = [
     [1, 5, 9],
     [3, 5, 7],
   ];
-  
+
   function movimento(e) {
     const index = e.target.getAttribute("data-jogodavelha");
     e.target.innerHTML = jogador;
@@ -41,3 +41,28 @@ let posicoes = [
   }
   
   inicio();
+
+  function quemganhou() {
+    let jogadorultimomovimento = jogador === "X" ? "O" : "X";
+  
+    const items = selecionado
+      .map((item, jogodavelha) => [item, jogodavelha])
+      .filter((item) => item[0] === jogadorultimomovimento)
+      .map((item) => item[1]);
+  
+
+    for (ganho of posicoes) {
+      if (ganho.every((item) => items.includes(item))) {
+        alert("O JOGADOR '" + jogadorultimomovimento + "' GANHOU GG!");
+        inicio();
+        return;
+      }
+    }
+    if (selecionado.filter((item) => item).length === 9) {
+      alert("VISH EMPATOU");
+      inicio();
+      return;
+    }
+
+  }
+  
