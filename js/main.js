af02c16693e83490661a3b1e7ca342459a3b9a63
pabloleonardoo <luckyxd136@gmail.com>
2023-09-02 19:40:15 -0300

    atualizando js
    criei variavel posiçoes para mostrar quais sao as possibilidades de ganhar segundo as  posiçoes e criei uma função que pega o data do botao la do html e pega a funçao do click e com set mostra quem ganhou , e riei outra funçao aonde o pega o botao que sera inicializado vazio para ser preenchido por x ou boinha

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ window.onload = () => {
 
 let selecionado;
 let jogador = "X";
+
+let posicoes = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  
+  function movimento(e) {
+    const index = e.target.getAttribute("data-jogodavelha");
+    e.target.innerHTML = jogador;
+    e.target.removeEventListener("click", movimento);
+    selecionado[index] = jogador;
+  
+    setTimeout(() => {
+      quemganhou();
+    }, [100]);
+    jogador = jogador === "X" ? "O" : "X";
+   
+  }
+
+  function inicio() {
+    selecionado = [];
+    document.querySelectorAll(".jogodavelhadopablo button").forEach((item) => {
+      item.innerHTML = "";
+      item.addEventListener("click", movimento);
+    });
+  }
+  
+  inicio();
